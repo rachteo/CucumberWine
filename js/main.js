44dad95a67ce7248e1a4cbe29b75f380eb9a63a9
rachteo <rachelhjteo@gmail.com>
2012-08-01 17:55:14 -0700

    changing background

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
+var bgarray;
+var currbg = 0;
+
 $(document).ready(function() {	
 	
-	  $('#soundtrack').css("width", $(window).width() - 425);	
+	$('#soundtrack').css("width", $(window).width() - 425);	
 	
+	bgarray = [ "01", "02" ];
+	
+	setInterval( function(){changeBackground()}, 5000 );
+
 });
+
+function changeBackground() {
+	
+	currbg++;
+	
+	if( currbg >= bgarray.length ) {
+		currbg = 0;
+	}
+	
+	$('#flickr').html( '<img class="flickrbg" src="./img/' + bgarray[ currbg ]  + '.jpg">' );
+	console.log( "background changing" );
+	
+}
+
+function addSong() {
+	$('#playlistform').css( "visibility", "visible" );
+}
+function submitForm() {
+	$('#playlistform').css( "visibility", "hidden" );	
+}
